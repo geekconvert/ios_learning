1ee02edd0b2ef07862e911f6974a4b45aaabfbc6
anish kumar <kumaranishdubey@gmail.com>
2026-02-12 15:24:24 +0530

    init

--- 5/Scrumdinger/DetailView.swift
+++ 5/Scrumdinger/DetailView.swift
@@ -46,21 +46,21 @@ struct DetailView: View {
                 isPresentingEditView = true
             }
         }
-        //Add a sheet modifier on List. When isPresentingEditView changes to true, the app presents DetailEditView using a modal sheet that partially covers the underlying content.
+        ///Add a sheet modifier on List. When isPresentingEditView changes to true, the app presents DetailEditView using a modal sheet that partially covers the underlying content. Modal views remove users from the main navigation flow of the app. Use modality for short, self-contained tasks.
         .sheet(isPresented: $isPresentingEditView){
             NavigationStack {
                 DetailEditView()
-                    //Set the navigation title of the edit view.
+                    ///Set the navigation title of the edit view.
                     .navigationTitle(scrum.title)
-                    //You need the toolbar to offer different actions when the user is editing a scrum than when they’re viewing a scrum.
+                    ///You need the toolbar to offer different actions when the user is editing a scrum than when they’re viewing a scrum.
                     .toolbar {
-                        //Add a toolbar button that indicates to the user that they’re canceling changes to the scrum details. Dismiss DetailEditView in the Cancel button action.
+                        ///Add a toolbar button that indicates to the user that they’re canceling changes to the scrum details. Dismiss DetailEditView in the Cancel button action.
                         ToolbarItem(placement: .cancellationAction){
                             Button("Cancel"){
                                 isPresentingEditView = false
                             }
                         }
-                        //Add a toolbar button that indicates to the user that they’re saving changes. Dismiss DetailEditView in the Done button action.
+                        ///Add a toolbar button that indicates to the user that they’re saving changes. Dismiss DetailEditView in the Done button action.
                         ToolbarItem(placement: .confirmationAction) {
                             Button("done") {
                                 isPresentingEditView = false
