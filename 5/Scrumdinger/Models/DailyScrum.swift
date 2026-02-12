5cf733e5da4a28fc61331df6c7717ca1c7f2c523
anish kumar <kumaranishdubey@gmail.com>
2026-02-12 15:11:08 +0530

    init

--- 5/Scrumdinger/Models/DailyScrum.swift
+++ 5/Scrumdinger/Models/DailyScrum.swift
@@ -10,13 +10,16 @@ struct DailyScrum: Identifiable {
     var title: String
     var attendees: [Attendee]
     var lengthInMinutes: Int
+    /// By using a computed property, you can provide a getter to retrieve the scrum’s length as a double value and a setter to update the corresponding integer value when the slider changes.
     var lengthInMinutesAsDouble: Double {
+        
+        /// For Swift functions that return a value, you can omit the return keyword when the body is a single expression.
         get {
             Double(lengthInMinutes)
         }
-        //You can provide an argument to the setter to name the newly set value. If you omit the argument, the setter uses newValue as the default name.
+        /// You can provide an argument to the setter to name the newly set value. If you omit the argument, the setter uses newValue as the default name.
         set{
-            //When the slider changes the value of lengthInMinutesAsDouble, convert the double value to an integer, and update the lengthInMinutes property.
+            /// When the slider changes the value of lengthInMinutesAsDouble, convert the double value to an integer, and update the lengthInMinutes property.
             lengthInMinutes = Int(newValue)
         }
     }
